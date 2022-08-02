ba2334d179239dd2fc65a6940b1d159e98ebf1c7
AsilGuneri <89381813+AsilGuneri@users.noreply.github.com>
2022-08-03 00:57:06 +0300

    .

--- Assets/Scripts/Enemies/BasicEnemyController.cs
+++ Assets/Scripts/Enemies/BasicEnemyController.cs
@@ -5,9 +5,13 @@ using UnityEngine.AI;
 
 public class BasicEnemyController : MonoBehaviour
 {
+    [SerializeField] private float range;
+
+
     protected NavMeshAgent _agent;
     protected Health _hc;
     protected GameObject _target;
+    protected EnemyAnimationController _eac;
 
     private TargetController _tc;
     private bool isChasing;
@@ -17,6 +21,7 @@ public class BasicEnemyController : MonoBehaviour
     {
         _tc = GetComponent<TargetController>();
         _agent = GetComponent<NavMeshAgent>();
+        _eac = GetComponent<EnemyAnimationController>();
     }
     private void FixedUpdate()
     {
@@ -24,9 +29,13 @@ public class BasicEnemyController : MonoBehaviour
     }
     protected void PickTarget()
     {
-        if (!_tc.Target) {
+        if (!_tc.Target)
+        {
             _tc.Target = UnitManager.Instance.GetClosestUnit(transform.position);
         }
+
+        else if (Vector2.Distance(Extensions.Vector3ToVector2(_tc.Target.transform.position), Extensions.Vector3ToVector2(transform.position)) < range)
+            Attack();
         else Chase();
     }
     public void Activate()
@@ -36,11 +45,26 @@ public class BasicEnemyController : MonoBehaviour
     protected void Chase()
     {
         if (!_tc.Target || isInAttackRange) return;
-        _agent.SetDestination(_tc.Target.transform.position);
+        _eac.OnRun();
+        Move(_tc.Target.transform.position);
+        transform.LookAt(new Vector3(_tc.Target.transform.position.x, transform.position.y, _tc.Target.transform.position.z));
         if (!isChasing) isChasing = true;
     }
     protected void Attack()
     {
+        transform.LookAt(new Vector3(_tc.Target.transform.position.x, transform.position.y, _tc.Target.transform.position.z));
+        StopMove();
+        _eac.OnAttack();
+    }
+    private void StopMove()
+    {
+        _agent.isStopped = true;
+        _agent.velocity = Vector3.zero;
+    }
+    private void Move(Vector3 destination)
+    {
+        _agent.isStopped = false;
+        _agent.SetDestination(destination);
 
     }
 }
