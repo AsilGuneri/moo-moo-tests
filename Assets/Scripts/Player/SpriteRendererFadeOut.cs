ce1d593129faac98067dd1a68129b2029c00e695
AsilGuneri <89381813+AsilGuneri@users.noreply.github.com>
2022-09-13 00:53:09 +0300

    vfx package

--- Assets/Scripts/Player/SpriteRendererFadeOut.cs
+++ Assets/Scripts/Player/SpriteRendererFadeOut.cs
@@ -5,35 +5,21 @@ using UnityEngine.UI;
 using DG.Tweening;
 using MyBox;
 
-[RequireComponent(typeof(SpriteRenderer))]
 public class SpriteRendererFadeOut : MonoBehaviour
 {
-    [SerializeField] private float time;
-    [SerializeField] private IndicatorGroup moveIndicator;
-    [SerializeField] private IndicatorGroup attackIndicator;
+    [SerializeField] private GameObject moveIndicator;
+    [SerializeField] private GameObject attackIndicator;
 
-    private Tween fadeReset;
-    private Tween fadeOut;
-
+    private GameObject _currentIndicator;
 
     public void Setup(Vector3 pos,bool isMove)
     {
-        var spriteRend = GetComponent<SpriteRenderer>();
-        spriteRend.sprite = isMove ? moveIndicator.IndicatorSprite : attackIndicator.IndicatorSprite;
-        spriteRend.color = isMove ? moveIndicator.IndicatorColor : attackIndicator.IndicatorColor;
+        _currentIndicator = isMove ? moveIndicator : attackIndicator;
 
-        fadeReset.Kill();
-        fadeOut.Kill();
         if (transform.parent != null) transform.parent = null;
         transform.position = new Vector3(pos.x, transform.position.y, pos.z);
-        fadeReset =  spriteRend.DOFade(1, 0);
-        fadeOut = spriteRend.DOFade(0, time);
+       _currentIndicator.GetComponent<ParticleSystem>().Play();
     }
 
 }
-[System.Serializable]
-public class IndicatorGroup
-{
-    public Sprite IndicatorSprite;
-    public Color IndicatorColor;
-}
+
