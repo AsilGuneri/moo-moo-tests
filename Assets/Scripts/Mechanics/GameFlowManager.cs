040724dd6dff20f09fdf341f11bd0980ac7983dd
AsilGuneri <89381813+AsilGuneri@users.noreply.github.com>
2023-11-18 21:58:51 +0300

    Update GameFlowManager.cs

--- Assets/Scripts/Mechanics/GameFlowManager.cs
+++ Assets/Scripts/Mechanics/GameFlowManager.cs
@@ -60,7 +60,7 @@ public class GameFlowManager : NetworkSingleton<GameFlowManager>
     IEnumerator StartGameRoutine()
     {
         yield return Extensions.GetWait(1);
-        //SetGameState(GameState.WaveCountdown);
+        SetGameState(GameState.WaveCountdown);//comment it to test waves manually
 
     }
     private void OnFree()
