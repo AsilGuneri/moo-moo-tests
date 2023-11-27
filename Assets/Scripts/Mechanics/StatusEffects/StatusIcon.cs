a088b5f40cf7ede5000aa4b4f24ba1af92df156a
AsilGuneri <89381813+AsilGuneri@users.noreply.github.com>
2023-11-27 15:26:04 +0300

    Revert "Revert "Revert "status UI update"""
    This reverts commit f53923e01cb8024a12f36736406a59fc6eab74bf.

--- Assets/Scripts/Mechanics/StatusEffects/StatusIcon.cs
+++ Assets/Scripts/Mechanics/StatusEffects/StatusIcon.cs
@@ -6,42 +6,9 @@ using TMPro;
 
 public class StatusIcon : MonoBehaviour
 {
-    [SerializeField] private Image[] icons;
-    [SerializeField] private Image fillImage;
-
-    private float totalTime = 0;
-    private float timeRemaining = 0;
-    private bool isRunning = false;
-
-    public void Setup(Sprite icon, float time)
+    [SerializeField] private Image iconImage;
+    public void ChangeIconSprite(Sprite icon)
     {
-        timeRemaining = time;
-        totalTime = time;
-        isRunning = true;
-        foreach (var image in icons)
-        {
-            image.sprite = icon;
-        }
+        iconImage.sprite = icon;
     }
-
-    private void Update()
-    {
-        if (isRunning)
-        {
-            timeRemaining -= Time.deltaTime;
-            fillImage.fillAmount = timeRemaining / totalTime;
-            if (timeRemaining <= 0)
-            {
-                EndTimer();
-            }
-        }
-    }
-    private void EndTimer()
-    {
-        isRunning = false;
-        timeRemaining = 0;
-        totalTime = 0;
-    }
-
-    
 }
--- Assets/Scripts/Mechanics/StatusEffects/StatusTimer.cs
+++ Assets/Scripts/Mechanics/StatusEffects/StatusTimer.cs
@@ -6,4 +6,38 @@ using UnityEngine.UI;
 public class StatusTimer : MonoBehaviour
 {
 
+    [SerializeField] private Image fillImage;
+
+    private float totalTime = 0;
+    private float timeRemaining = 0;
+    private bool isRunning = false;
+
+    private void Update()
+    {
+        if (isRunning)
+        {
+            timeRemaining -= Time.deltaTime;
+            fillImage.fillAmount = timeRemaining / totalTime;
+            if (timeRemaining <= 0)
+            {
+                EndTimer();
+            }
+        }
+    }
+
+    private void EndTimer()
+    {
+        isRunning = false;
+        timeRemaining = 0;
+        totalTime = 0;
+    }
+
+    public void StartTimer(float time, Color color = default)
+    {
+        if (color == default) color = Color.white;
+        fillImage.color = color;
+        timeRemaining = time;
+        totalTime = time;
+        isRunning = true;
+    }
 }
