f53923e01cb8024a12f36736406a59fc6eab74bf
AsilGuneri <89381813+AsilGuneri@users.noreply.github.com>
2023-11-27 14:55:40 +0300

    Revert "Revert "status UI update""
    This reverts commit 67d91288c01533ab8efa6f1f3d42f79eda6ce11d.

--- Assets/Scripts/Mechanics/StatusEffects/StatusIcon.cs
+++ Assets/Scripts/Mechanics/StatusEffects/StatusIcon.cs
@@ -6,9 +6,42 @@ using TMPro;
 
 public class StatusIcon : MonoBehaviour
 {
-    [SerializeField] private Image iconImage;
-    public void ChangeIconSprite(Sprite icon)
+    [SerializeField] private Image[] icons;
+    [SerializeField] private Image fillImage;
+
+    private float totalTime = 0;
+    private float timeRemaining = 0;
+    private bool isRunning = false;
+
+    public void Setup(Sprite icon, float time)
     {
-        iconImage.sprite = icon;
+        timeRemaining = time;
+        totalTime = time;
+        isRunning = true;
+        foreach (var image in icons)
+        {
+            image.sprite = icon;
+        }
     }
+
+    private void Update()
+    {
+        if (isRunning)
+        {
+            timeRemaining -= Time.deltaTime;
+            fillImage.fillAmount = timeRemaining / totalTime;
+            if (timeRemaining <= 0)
+            {
+                EndTimer();
+            }
+        }
+    }
+    private void EndTimer()
+    {
+        isRunning = false;
+        timeRemaining = 0;
+        totalTime = 0;
+    }
+
+    
 }
